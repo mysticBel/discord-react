1a563fba3e1391a3d283109cecc5e67cff68bc7d
mysticBel <dnxbel@gmail.com>
2021-11-04 19:13:09 -0500

    importing firebase credentials to project

---
+++ src/firebase/credentials.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCL0FApIClHKIsd8fWUEq46DY3KKkGbzk4",
+  authDomain: "discord-react-f8f3d.firebaseapp.com",
+  projectId: "discord-react-f8f3d",
+  storageBucket: "discord-react-f8f3d.appspot.com",
+  messagingSenderId: "410078028541",
+  appId: "1:410078028541:web:133135cb9c9c713de01494"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
