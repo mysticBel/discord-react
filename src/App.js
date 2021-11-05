d4fee826b4c9c4f94cc017f4bd207cbf9f0a1f2b
mysticBel <dnxbel@gmail.com>
2021-11-04 21:56:44 -0500

    importing components. login with Google works !

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React , { useState } from 'react';
 import firebaseApp from './firebase/credentials';
+import Login from './views/Login.js';
+import ChatScreen from './views/ChatScreen.js';
+import Sidebar from './views/Sidebar.js';
 
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const auth = getAuth(firebaseApp);
 
 function App() {
+  const [userGlobal, setUserGlobal] = useState(null);
+  const [channelActive, setchannelActive] = useState(null);
+
+  onAuthStateChanged(auth, (userFirebase) => {
+    if (userFirebase) {
+      setUserGlobal(userFirebase);
+      console.log("you have signed in succesfully !");
+    } else {
+      setUserGlobal(null);
+    }
+  });
+
   return (
-    <div>
-     <p style={{ color: "black" }}>Hello world ! 🦄✨</p>
-      <button> this is a cool button</button>
+    <div className="app">
+      {userGlobal ? (
+        <>
+          {" "}
+          <Sidebar
+            userGlobal={userGlobal}
+            setchannelActive={setchannelActive}
+          />{" "}
+          <ChatScreen channelActive={channelActive} user={userGlobal} />{" "}
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
