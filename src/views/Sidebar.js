11b948c45ce18de71a771370c253b9bec9620d2f
mysticBel <dnxbel@gmail.com>
2022-01-02 10:35:32 -0500

    import Firebase, adding logout functionality, display name and turn uid to #number

--- src/views/Sidebar.js
+++ src/views/Sidebar.js
@@ -1,13 +1,35 @@
 import React , { useState, useEffect } from 'react';
 import { Avatar } from '@material-ui/core';
-import { ExpandMore, Mic, Add, Headser, Settings } from '@material-ui/icons';
+import { ExpandMore, Mic, Add, Headset, Settings } from '@material-ui/icons';
 
-function Sidebar(){
+import firebaseApp from "../firebase/credentials";
+import { getAuth, signOut } from "firebase/auth";
+import { getFirestore, collection, doc, getDocs, setDoc  } from "firebase/firestore";
+
+const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+
+
+
+
+function Sidebar({ userGlobal, setchannelActive }){
+
+
+
+  function convertLetterToNumber(str) {
+    var out = 0, len = str.length;
+    for (let pos = 0; pos < len; pos++) {
+      out += (str.charCodeAt(pos) - 64) * Math.pow(26, len - pos - 1);
+    }
+    return out;
+  }
+
+
     return (
         <div className="sidebar">
 
           <div className="sidebar__top">
-            <h3> Server </h3>  
+            <h3> ✨ My server  🦄 </h3>  
             < ExpandMore />
           </div> 
 
@@ -30,20 +52,18 @@ function Sidebar(){
             </div>
 
             <div className="sidebar__profile">
-              <Avatar />
+              <Avatar src= { userGlobal.photoURL }/>
               <div className="sidebar__profileInfo">
-                <h3> Name </h3>
-                <p>#number</p>
-
+                <h3> { userGlobal.displayName } </h3>
+                <p>#{ convertLetterToNumber(userGlobal.uid.substring(0,2))}</p>
               </div>
-
-
+              <div className="sidebar__profileIcons">
+                <Mic />
+                <Headset />
+                <Settings onClick={() => signOut(auth)} />
+              </div>
             </div>
-
           </div> 
-
-
-
         </div>
     );
 }
