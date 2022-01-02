bcc698cf579a6e5a2b0ce921f7ae22fd55bebf8e
mysticBel <dnxbel@gmail.com>
2022-01-02 09:20:21 -0500

    adding  sidebar

--- src/views/Sidebar.js
+++ src/views/Sidebar.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
+import { Avatar } from '@material-ui/core';
+import { ExpandMore, Mic, Add, Headser, Settings } from '@material-ui/icons';
 
 function Sidebar(){
     return (
-        <div>
-          Sidebar component works ! ✨
+        <div className="sidebar">
+
+          <div className="sidebar__top">
+            <h3> Server </h3>  
+            < ExpandMore />
+          </div> 
+
+          <div className="sidebar__channels">
+            <div className="sidebar__channelsHeader">
+              <div className="sidebar__header">
+                <ExpandMore />
+                <h4>CHAT</h4>
+              </div>
+
+              <Add className="sidebar__addChannel" />
+            </div>
+
+            <div className="sidebar__channelsList">
+              
+               {/* { listChannels ? listChannels.map((channel) => {
+
+               }): null} */}
+
+            </div>
+
+            <div className="sidebar__profile">
+              <Avatar />
+              <div className="sidebar__profileInfo">
+                <h3> Name </h3>
+                <p>#number</p>
+
+              </div>
+
+
+            </div>
+
+          </div> 
+
+
+
         </div>
-    )
+    );
 }
 
 export default Sidebar;
