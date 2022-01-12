06a07b15716e57bccb0fad44e921457422ad4519
mysticBel <dnxbel@gmail.com>
2022-01-12 16:39:23 -0500

    add cute images in login <3

--- src/views/Login.js
+++ src/views/Login.js
@@ -13,12 +13,18 @@ function Login() {
 
   return (
     <div className="login">
-      <div className="login__logo">
-        <img src="https://i.scdn.co/image/ab67706c0000bebb89ee2d1d4947372cfbd39083" alt="" />
+
+      <div className="login__logo">   
+        <img src="https://raw.githubusercontent.com/mysticBel/discord-react/adbd943854ae4a93de262108a6b305a8e13ce37d/src/assets/imag-title-logo.png" alt="discord__logo" />
+        <img src="https://raw.githubusercontent.com/mysticBel/discord-react/adbd943854ae4a93de262108a6b305a8e13ce37d/src/assets/imag-logo-animal.png" alt="" />
+        {/* <img src="https://1000marcas.net/wp-content/uploads/2020/11/Discord-logo.png" alt="discord__logo" /> */}
+        {/* <img src="https://i.scdn.co/image/ab67706c0000bebb89ee2d1d4947372cfbd39083" alt="" /> */}
       </div>
       <Button onClick={logInGoogle}>Enter with Google</Button>
     </div>
+    
   );
 }
 
 export default Login;
+
